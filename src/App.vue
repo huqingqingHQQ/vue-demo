76c20fd3ab5b5be16a839f12ef169daf5584c603
huqingqingHQQ <1228322879@qq.com>
2018-03-14 23:19:38 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods" class="link">商品</router-link>
@@ -18,12 +18,21 @@
 
 <script>
   import header from './components/header/header.vue';
-
+  const ERR_OK = 0;
   export default {
     data() {
       return {
         seller: {}
       };
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+          if(response.errno === ERR_OK){
+            this.seller = response.data;
+            console.log(this.seller);
+          }
+      });
     },
     components: {
       'v-header': header
--- src/main.js
+++ src/main.js
@@ -10,13 +10,23 @@ import './commen/stylus/index.styl';
 //开启debug模式
 Vue.config.debug = true;
 
+//告诉vue要使用VueResource
+Vue.use(VueResource);
 //现在我们可以启动应用了
 //路由器会创建一个app实例，并且挂载到选择符#app匹配的元素上
 
 new Vue({
 	router,
-	render:h=> h(App)
+	render:h=> h(App),
+	mounted () {
+        this.getHttp('/')
+    },
+    methods: {
+        getHttp (url) {
+            this.$http
+                .get(url)
+                .then(response => console.log(response))
+                .catch(err => console.error(err))
+        },
+    }
 }).$mount('#app');
-
-//告诉vue要使用VueResource
-Vue.use(VueResource);
