dcd39a332a6b28e00e0b0b0156145319828dd4b2
huqingqingHQQ <1228322879@qq.com>
2018-03-27 21:41:16 +0800

    加减

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -28,19 +28,24 @@
   								<span class="now">￥{{food.price}}</span>
   								<span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
   							</div>
+  							<div class="cartcontrol-wrapper">
+  								<cartcontrol :food="food"></cartcontrol>
+  							</div>
+  							
   						</div>
   					</li>
   				</ul>
   			</li>
   		</ul>
   	</div>
-  	<shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+  	<shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
 <script>
 	import BScroll from "better-scroll"
 	import shopcart from "../../components/shopcart/shopcart.vue";
+	import cartcontrol from "../../components/cartcontrol/cartcontrol.vue";
 
 	const ERR_OK = 0;
 	export default {
@@ -66,6 +71,17 @@
 	    			}
 	    		}
 	    		return 0;
+	    	},
+	    	selectFoods() {
+	    		let foods = [];
+	    		this.goods.forEach((good) => {
+	    			good.foods.forEach((food) => {
+	    				if (food.count) {
+	    					foods.push(food);
+	    				}
+	    			});
+	    		});
+	    		return foods;
 	    	}
 	    },
 	  	created() {
@@ -91,12 +107,16 @@
 	  			let el = foodList[index];
 	  			this.foodsScroll.scrollToElement(el,300);
 	  		},
+	  		_drop(target) {
+	  			
+	  		},
 	  		_initScroll() {
 	  			this.menuScroll = new BScroll(this.$refs.menuWrapper, {
 	  				click: true
 	  			});
 
 	  			this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+	  				click: true,
 	  				probeType: 3
 	  			});
 
@@ -116,7 +136,13 @@
 	  		}
 	  	},
 	  	components: {
-	  		shopcart
+	  		shopcart,
+	  		cartcontrol
+	  	},
+	  	events: {
+	  		'cart.add'(target) {
+	  			this._drop(target);
+	  		}
 	  	}
 	};
 </script>
@@ -188,6 +214,7 @@
 				margin: 18px
 				padding-bottom: 18px
 				border-1px(rgba(7,17,27,0.1))
+				position: relative
 				&:last-child
 					border-none()
 					margin-bottom: 0
@@ -224,4 +251,8 @@
 							font-size: 10px;
 							color: rgb(147,153,159)
 							text-decoration: line-through
+					.cartcontrol
+						position: absolute
+						right: 0
+						bottom: 12px
 </style>
--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -12,8 +12,15 @@
   			<div class="desc">另需配送费￥{{deliveryPrice}}元</div>
   		</div>
   		<div class="content-right">
-  			<div class="pay">￥{{minPrice}}元起送</div>
+  			<div class="pay" :class="payClass">{{payDesc}}</div>
   		</div>
+  	</div>
+  	<div class="ball-container">
+  		<transition name="drop">
+  			<div v-for="ball in balls" v-show="ball.show" class="ball">
+  				<div class="inner"></div>
+  			</div>
+  		</transition>
   	</div>
   </div>
 </template>
@@ -41,6 +48,27 @@
 				default: 0
 			}
 		},
+		data() {
+			return {
+				balls: [
+					{
+						show: false
+					},
+					{
+						show: false
+					},
+					{
+						show: false
+					},
+					{
+						show: false
+					},
+					{
+						show: false
+					}
+				]
+			};
+		},
 		computed: {
 			totalPrice() {
 				let total = 0;
@@ -55,6 +83,29 @@
 					count += food.count; 
 				});
 				return count;
+			},
+			payDesc() {
+				if (this.totalPrice === 0) {
+					return `￥${this.minPrice}元起送`;
+				}else if ( this.totalPrice < this.minPrice ) {
+					let diff = this.minPrice - this.totalPrice;
+					return `还差￥${diff}元`;
+				}else {
+					return '去结算';
+				}
+			},
+			payClass() {
+				if ( this.totalPrice < this.minPrice ) {
+					let diff = this.minPrice - this.totalPrice;
+					return 'not-enough';
+				}else {
+					return 'enough';
+				}
+			}
+		},
+		methods: {
+			drop(el) {
+
 			}
 		}
 	};
@@ -143,5 +194,25 @@
 					text-align: center
 					font-size: 12px
 					font-weight: 700
-					background: #2b333b
+					&.not-enough
+						background: #2b333b
+					&.enough
+						background: #00b43c
+						color: #fff
+		.ball-container
+			.ball
+				position: fixed
+				left: 32px
+				bottom: 22px
+				z-index: 200
+				transition: all 0.4s
+				&.drop-enter,&.drop-leave-active
+					.inner
+						width: 16px
+						height: 16px
+						border-radius: 50%
+						background: rgb(0,160,220)
+						transition: all 0.4s
+
+
 </style>
